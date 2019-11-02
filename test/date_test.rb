6522a8a2a5d8f323a211fa5692863d0f3192e1e4
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-11-02 15:45:57 -0600

    Add stubs for date test to get the same date every day

--- test/date_test.rb
+++ test/date_test.rb
@@ -1,5 +1,6 @@
 require_relative './test_helper'
 require './lib/date'
+require 'mocha/minitest'
 require 'Date'
 
 class OffsetTest < Minitest::Test
@@ -12,19 +13,23 @@ class OffsetTest < Minitest::Test
   end
 
   def test_it_can_get_current_date
+    @offset.stubs(:current_date).returns("021119")
     assert_equal "021119", @offset.current_date
   end
 
   def test_it_can_square_date
+    @offset.stubs(:current_date).returns("021119")
     assert_equal 446012161, @offset.square_date
   end
 
   def test_it_can_take_last_4_of_date_squared_as_array
+    @offset.stubs(:current_date).returns("021119")
     expected = [4,4,6,0,1,2,1,6,1]
     assert_equal expected, @offset.split_date_squared_to_array
   end
 
   def test_it_can_get_four_offsets
+    @offset.stubs(:current_date).returns("021119")
     assert_equal [2,1,6,1], @offset.offset_array
   end
 end
