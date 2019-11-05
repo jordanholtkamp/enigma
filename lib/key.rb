00a0a756bee812679e9ab24ea51a50aa129dbe6b
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-11-04 18:05:36 -0700

    Make key class only make a key

--- lib/key.rb
+++ lib/key.rb
@@ -1,21 +1,22 @@
 class Key
 
-  attr_reader :key_array
-  def initialize
-    @key_array = []
+  attr_reader :key#, :key_array
+  def initialize #(given_key = random_number_string)
+    @key = random_number_string
+    # @key_array = []
   end
 
-  def random_number_string
+  def self.random_number_string
     5.times.map { rand(9) }.join
   end
-
-  def array_of_five_digits
-    random_number_string.split("")
-  end
-
-  def make_four_keys
-    random_number_array.each_cons(2) do |a,b|
-      @key_array << (a + b).to_i
-    end
-  end
+  #
+  # def array_of_five_digits
+  #   key.split("")
+  # end
+  #
+  # def make_four_keys
+  #   array_of_five_digits.each_cons(2) do |a,b|
+  #     @key_array << (a + b).to_i
+  #   end
+  # end
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -27,7 +27,7 @@ class KeyTest < Minitest::Test
   end
 
   def test_key_array_has_4_ints
-    @key.stubs(:random_number_array).returns(["1", "2", "4", "1", "3"])
+    @key.stubs(:key).returns("12413")
     @key.make_four_keys
     assert_equal [12,24,41,13], @key.key_array
   end
