8d55092e1aa7a48220fcec2de053156b42e0831d
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-11-04 17:01:02 -0700

    Add functionality to take date arg and give default current date

--- lib/date.rb
+++ lib/date.rb
@@ -2,21 +2,21 @@ require 'Date'
 
 class Offset
 
-  attr_reader :offset_array
-  def initialize
-    @date = Time.now
+  attr_reader :offset_array, :date
+  def initialize(given_date = current_date)
+    @date = given_date
     @offset_array = []
   end
 
   def current_date
-    day = @date.day.to_s.rjust(2, "0")
-    month = @date.month.to_s.rjust(2, "0")
-    year = @date.year.to_s.slice(2,3)
+    day = Time.now.day.to_s.rjust(2, "0")
+    month = Time.now.month.to_s.rjust(2, "0")
+    year = Time.now.year.to_s.slice(2,3)
     day + month + year
   end
 
   def square_date
-    current_date.to_i**2
+    date.to_i**2
   end
 
   def split_date_squared_to_array
@@ -25,10 +25,10 @@ class Offset
   end
 
   def create_offsets
-    a_offset = split_date_squared_to_array[5]
-    b_offset = split_date_squared_to_array[6]
-    c_offset = split_date_squared_to_array[7]
-    d_offset = split_date_squared_to_array[8]
+    a_offset = split_date_squared_to_array[-4]
+    b_offset = split_date_squared_to_array[-3]
+    c_offset = split_date_squared_to_array[-2]
+    d_offset = split_date_squared_to_array[-1]
     @offset_array = [a_offset, b_offset, c_offset, d_offset]
   end
 end
--- test/date_test.rb
+++ test/date_test.rb
@@ -6,26 +6,33 @@ require 'Date'
 class OffsetTest < Minitest::Test
   def setup
     @offset = Offset.new
+    @offset_2 = Offset.new("040895")
   end
 
   def test_it_exists
     assert_instance_of Offset, @offset
+    assert_instance_of Offset, @offset_2
   end
 
   def test_it_can_get_current_date
     @offset.stubs(:current_date).returns("021119")
     assert_equal "021119", @offset.current_date
+    assert_equal "040895", @offset_2.date
   end
 
   def test_it_can_square_date
-    @offset.stubs(:current_date).returns("021119")
+    @offset.stubs(:date).returns("021119")
     assert_equal 446012161, @offset.square_date
+    assert_equal 1672401025, @offset_2.square_date
   end
 
   def test_it_can_make_array_from_squared_value
-    @offset.stubs(:current_date).returns("021119")
+    @offset.stubs(:date).returns("021119")
     expected = [4,4,6,0,1,2,1,6,1]
     assert_equal expected, @offset.split_date_squared_to_array
+
+    expected_2 = [1,6,7,2,4,0,1,0,2,5]
+    assert_equal expected_2, @offset_2.split_date_squared_to_array
   end
 
   def test_it_can_get_four_offsets
