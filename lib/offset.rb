c36852d0f515402ba465026f6223e05e4f36e33c
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-11-05 13:45:14 -0700

    Make current date method cleaner with strftime

--- lib/offset.rb
+++ lib/offset.rb
@@ -2,9 +2,6 @@ require 'Date'
 
 class Offset
   def self.current_date
-    day = Time.now.day.to_s.rjust(2, "0")
-    month = Time.now.month.to_s.rjust(2, "0")
-    year = Time.now.year.to_s.slice(2,3)
-    day + month + year
+    Time.now.strftime("%d%m%y")
   end
 end
