5312b2dd4f65a4fd30797e0c575b7a3da2f96dc0
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-11-04 18:04:52 -0700

    Refactor date to make only responsibility to create date

--- lib/date.rb
+++ lib/date.rb
@@ -2,33 +2,33 @@ require 'Date'
 
 class Offset
 
-  attr_reader :offset_array
-  def initialize
-    @date = Time.now
-    @offset_array = []
+  attr_reader :date   #, :offset_array
+  def initialize #(given_date = current_date)
+    @date = current_date
+    # @offset_array = []
   end
 
-  def current_date
-    day = @date.day.to_s.rjust(2, "0")
-    month = @date.month.to_s.rjust(2, "0")
-    year = @date.year.to_s.slice(2,3)
+  def self.current_date
+    day = Time.now.day.to_s.rjust(2, "0")
+    month = Time.now.month.to_s.rjust(2, "0")
+    year = Time.now.year.to_s.slice(2,3)
     day + month + year
   end
 
-  def square_date
-    current_date.to_i**2
-  end
-
-  def split_date_squared_to_array
-    string_array = square_date.to_s.split("")
-    string_array.map { |string| string.to_i }
-  end
-
-  def create_offsets
-    a_offset = split_date_squared_to_array[5]
-    b_offset = split_date_squared_to_array[6]
-    c_offset = split_date_squared_to_array[7]
-    d_offset = split_date_squared_to_array[8]
-    @offset_array = [a_offset, b_offset, c_offset, d_offset]
-  end
+  # def square_date
+  #   date.to_i**2
+  # end
+  #
+  # def split_date_squared_to_array
+  #   string_array = square_date.to_s.split("")
+  #   string_array.map { |string| string.to_i }
+  # end
+  #
+  # def create_offsets
+  #   a_offset = split_date_squared_to_array[-4]
+  #   b_offset = split_date_squared_to_array[-3]
+  #   c_offset = split_date_squared_to_array[-2]
+  #   d_offset = split_date_squared_to_array[-1]
+  #   @offset_array = [a_offset, b_offset, c_offset, d_offset]
+  # end
 end
